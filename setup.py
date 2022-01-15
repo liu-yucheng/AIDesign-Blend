08cc40e11a5ee4bdab3d6384a9c408d999da8120
liu-yucheng <liu.r.yucheng@gmail.com>
2022-01-16 00:16:52 +0800

    Fixed readme

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def main():
     """Main function."""
     _setup(
         name="aidesign-blend",
-        version="0.9.6",
+        version="0.9.7",
         description="AIDesign Image Fragments Blending Application",
         author="Yucheng Liu (From The AIDesign Team)",
         packages=_find_packages(),
